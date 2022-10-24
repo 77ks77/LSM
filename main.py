a3903a1f41f0ff0cf60435ca55479c20c04c1bba
77ks77 <116544656+77ks77@users.noreply.github.com>
2022-10-24 15:24:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("lsm")
+import streamlit as st
+
+st.title('Hello streamlit.')
+counter = 0
+
+increment = st.button('Increment')
+if increment:
+    counter += 1
+
+st.write('Count= ', counter)
